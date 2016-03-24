b54b340b3691a6b39fa0f81cfae3330b2e551e5f
Dan Roundhill <dan@automattic.com>
2016-03-24 14:11:46 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# simplenote-electron-downloads
+# Simplenote Electron Downloads
+
+All the Simplenote electron download counts you need in one place!
---
+++ app.js
@@ -0,0 +1,72 @@
+const apiEndpoint = 'https://api.github.com/repos/automattic/simplenote-electron/releases';
+
+const SimplenoteDownload = React.createClass( {
+	getDefaultProps: function() {
+		return {
+			appData: {
+				name: '',
+				download_count: 0
+			}
+		};
+	},
+
+	render: function() {
+		const { name } = this.props.appData;
+		var platform;
+		if ( name.indexOf( 'exe' ) > -1 ) {
+			platform = 'windows';
+		} else if ( name.indexOf( 'deb' ) > -1 || name.indexOf( 'tar' ) > -1 ) {
+			platform = 'linux';
+		} else {
+			platform = 'osx';
+		}
+
+		const imageSrc = `images/${platform}.png`;
+
+		return (
+			<div className="simplenote-app">
+				<img src={ imageSrc } />
+				<div className="title">{ this.props.appData.name }</div>
+				<div className="downloads">{ this.props.appData.download_count.toLocaleString() }</div>
+			</div>
+		);
+	}
+});
+
+const App = React.createClass({
+
+	getInitialState: function() {
+		return {
+			apiData: []
+		}
+	},
+
+	componentDidMount: function() {
+		this.serverRequest = $.get( apiEndpoint, function( result ) {
+	      this.setState( {
+	        apiData: result[ 0 ][ 'assets' ]
+	      } );
+	    }.bind( this ) );
+	},
+
+	render: function() {
+		return (
+			<div className="app">	
+				<img className="simplenote-logo" src="images/icon.png" />
+				<div className="greetz">Download Counts</div>
+				<div className="simplenote-apps">
+					{this.state.apiData.map( appData => {
+						return (
+							<SimplenoteDownload appData={ appData } />
+						);
+					} )}
+				</div>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('content')
+);
